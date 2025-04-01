6cb1476bf124b440344ffd3b317e2b259fd0b492
WonterRobter <wouterke.delaet@telenet.be>
2025-04-01 14:27:45 +0200

    venv enzo

---
+++ fetch_battle_cats.py
@@ -0,0 +1,15 @@
+import requests
+
+API_URL = "https://my-json-server.typicode.com/WonterRobter/battle-cats-enemies/enemies"
+
+def fetch_data():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        data = response.json()
+        print("Battle Cats Enemies")
+        for item in data:
+            print(f"ID: {item['id']}, Naam: {item['name']}, Type: {item['type']}, Health: {item['health']}, Attack: {item['attack']}")
+    else:
+        print("❌ Fout bij ophalen van data")
+
+fetch_data()
