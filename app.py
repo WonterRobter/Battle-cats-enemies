ca35530bdd01b780297c43bfb1cf9f5742b08ff9
WonterRobter <wouterke.delaet@telenet.be>
2025-04-04 12:08:45 +0200

    .

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # BRONNEN: https://battlecats.miraheze.org/wiki/Enemy_Dictionary (1/04/2025)
-#          https://chatgpt.com/ (01/04/2025) --> gebruikt om te filteren uit githubfile
-#          
+#          https://chatgpt.com/ (01/04/2025) --> gebruikt om te filteren uit githubfile en ordenen.
 #          https://github.com/TMZ-429/bcc/blob/main/enemies.json (31/03/2025) --> inspiratie
-#          https://my-json-server.typicode.com/WonterRobter/battle-cats-enemies
 #          https://www.w3schools.com/html/html_tables.asp (04/04/2025)
 #               https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_intro (04/04/2025)
 #               https://www.youtube.com/watch?v=e62D-aayveY&list=PLP9IO4UYNF0VdAajP_5pYG-jG2JRrG72s (04/04/2025)
+#       De URL van mijn GitHub-repository met het JSON-bestand.
+#           https://my-json-server.typicode.com/WonterRobter/battle-cats-enemies
 
 
 
